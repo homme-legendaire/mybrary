4388c24485d95916f869bfeb79171311a5b9993d
SooYoung Lee <jaylee2366@gmail.com>
2024-05-01 15:32:08 +0900

    for merge

--- src/components/modal/SelectedBookControlModal.js
+++ src/components/modal/SelectedBookControlModal.js
@@ -1,15 +1,17 @@
-import { Modal, IconButton } from "@mui/material";
+import { Modal, IconButton, Button } from "@mui/material";
 import styles from "./SelectedBookControlModal.module.css";
-import { AddAPhoto, BookmarkAdd, IosShare } from "@mui/icons-material";
+import { AddAPhoto, BookmarkAdd, Delete, IosShare } from "@mui/icons-material";
 import { useState } from "react";
 import BookMarkModal from "./BookMarkModal";
 import BookMarkAddModal from "./BookMarkAddModal";
 import { useRecoilState } from "recoil";
-import { bookMarkState } from "../recoil/atom";
+import { bookMarkState, savedBookListState } from "../recoil/atom";
+import { parseCookies } from "nookies";
 
 export default function SelectedBookControlModal({ open, onClose, book }) {
   const [bookMark, setBookMark] = useRecoilState(bookMarkState);
 
+  console.log("BOOK", book);
   console.log("BOOKMARK", bookMark);
 
   // 책갈피 모달 변수
@@ -19,8 +21,39 @@ export default function SelectedBookControlModal({ open, onClose, book }) {
   const [bookMarkAddModalOpen, setBookMarkAddModalOpen] = useState(false);
   const [bookMarkAddModalText, setBookMarkAddModalText] = useState("");
 
+  // 책 삭제 모달 변수
+  const [bookDeleteModalOpen, setBookDeleteModalOpen] = useState(false);
+  const [savedBookList, setSavedBookList] = useRecoilState(savedBookListState);
+
   const bookMarkModalOpenHandler = (key) => {
     setBookMarkModalOpen(true);
+  };
+
+  const bookDeleteHandler = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.PRODUCTION_SERVER_HOST}/deleteBook`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            token: parseCookies(null, "token").token,
+            book: book.docId,
+          },
+        }
+      );
+      const resJson = await res.json();
+      if (resJson.result === "success") {
+        setBookDeleteModalOpen(false);
+        setSavedBookList(
+          savedBookList.filter((item) => item.title !== book.title)
+        );
+      } else {
+        alert("서버에서 오류가 발생했습니다.");
+      }
+    } catch (err) {
+      alert(`에러가 발생했습니다. ${err}`);
+    }
   };
 
   return (
@@ -33,6 +66,46 @@ export default function SelectedBookControlModal({ open, onClose, book }) {
         open={bookMarkAddModalOpen}
         onClose={() => setBookMarkAddModalOpen(false)}
       />
+      <Modal
+        open={bookDeleteModalOpen}
+        onClose={() => setBookDeleteModalOpen(false)}
+      >
+        <div className={styles.smallModal}>
+          <span className={styles.title}>책을 삭제하시겠습니까?</span>
+          <div className={styles.modalBtn}>
+            <Button
+              fullWidth
+              sx={{
+                fontSize: "1.25rem",
+                color: "#ffffff",
+                backgroundColor: "primary.main",
+                "&:hover": {
+                  backgroundColor: "primary.dark",
+                },
+              }}
+              onClick={bookDeleteHandler}
+            >
+              삭제
+            </Button>
+            <Button
+              fullWidth
+              sx={{
+                fontSize: "1.25rem",
+                color: "#ffffff",
+                backgroundColor: "#222849",
+                "&:hover": {
+                  backgroundColor: "#272200",
+                },
+              }}
+              onClick={() => {
+                setBookDeleteModalOpen(false);
+              }}
+            >
+              취소
+            </Button>
+          </div>
+        </div>
+      </Modal>
       <Modal open={open} onClose={onClose} disableAutoFocus>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
@@ -52,15 +125,20 @@ export default function SelectedBookControlModal({ open, onClose, book }) {
               </div>
             </div>
             <div className={styles.bookActionBtn}>
-              <IconButton
-                onClick={() => {
-                  setBookMarkAddModalOpen(true);
-                }}
-              >
-                <AddAPhoto />
-              </IconButton>
+              {Object.keys(bookMark).length === 0 && (
+                <IconButton
+                  onClick={() => {
+                    setBookMarkAddModalOpen(true);
+                  }}
+                >
+                  <AddAPhoto />
+                </IconButton>
+              )}
               <IconButton>
                 <IosShare />
+              </IconButton>
+              <IconButton onClick={() => setBookDeleteModalOpen(true)}>
+                <Delete />
               </IconButton>
             </div>
           </div>
